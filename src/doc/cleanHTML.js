fbe969a23734a072f3d8ce4f702f94222ea94af3
Jack <mail@jackarrington.com>
2019-10-29 16:43:17 -0400

    Paste cleanup, preserving font weight and style, nothing else

--- src/doc/cleanHTML.js
+++ src/doc/cleanHTML.js
@@ -3,39 +3,37 @@ const cleanHTML = (html) => {
   const pasted = document.createElement('html')
   pasted.innerHTML = html
 
-  const cleaned = stripUnwanted(pasted)
+  pasted.querySelectorAll('[style]').forEach(e => {
+    const weight = fontWeight(e.style['font-weight'])
+    const style = fontStyle(e.style['font-style'])
 
-  return pasted.innerText
+    e.style = ''
+    e.style['font-weight'] = weight
+    e.style['font-style'] = style
+  });
+
+  return pasted.innerHTML
 }
 
-const stripUnwanted = (e) => {
-  let innerHTML = ''
-  if (e.children.length < 1) {
-    innerHTML = e.innerHTML
-  }
-  else {
-    for (const child of e.children) {
-      innerHTML += stripUnwanted(child)
+const fontWeight = (weight) => {
+  const number = Number(weight)
+  if (number) {
+    if (number <= 400) {
+      return 'normal'
+    }
+    else {
+      return 'bold'
     }
   }
-
-  if (['SPAN', 'BODY', 'B'].includes(e.tagName)) {
-    return innerHTML
+  else {
+    if (weight === 'lighter') {
+      return 'normal'
+    }
+    return weight
   }
-  else if (e.tagName === 'BR') {
-    return ''
-  }
-
-  const tag = e.tagName.toLowerCase()
-  return `<${tag}>${innerHTML}</${tag}>`
 }
 
-// const applyStyles = (innerHTML, styles) => {
-//   const asArray = Array.from(styles)
-//   const openingTags = asArray.map(tag => `<${tag}>`).join('')
-//   const closingTags = asArray.map(tag => `</${tag}>`).join('')
 
-//   return openingTags + innerHTML + closingTags
-// }
+const fontStyle = (style) => style === 'italic' ? style : 'normal'
 
 export default cleanHTML
